27fe50be4cd1ab7719e234519f07983108ebfc9b
Yuta Toyokawa <kppg42@gmail.com>
2023-03-01 08:24:38 +0900

    インスタアイコンの追加

--- components/globals/Header.vue
+++ components/globals/Header.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
 header
   .container
-    nuxt-link.logo(to="/")
-      img(src="@/assets/images/logo.png" alt="彩園なかや" width="50" height="40")
+    .header-nav
+      nuxt-link.logo(to="/")
+        img(src="@/assets/images/logo.png" alt="彩園なかや" width="50" height="40")
+      a.instaicon(href="https://www.instagram.com/saiennakaya/" target="_blank" rel="noopener")
+        img(src="@/assets/images/instaicon.svg" alt="彩園なかやインスタグラム" width="30" height="30")
 </template>
 
 <style lang="sass" scoped>
@@ -11,12 +14,17 @@ header
   background-color: $white
   border-bottom: 1px solid $grey
   border-top: 5px solid $main
-  padding: $block-padding
-  display: flex
-  align-items: center
-  justify-content: flex-start
+  padding: 13px 0
+  .header-nav
+    display: flex
+    align-items: center
+    justify-content: space-between
   .logo
     img
       height: 40px
+      width: auto
+  .instaicon
+    img
+      height: 30px
       width: auto
 </style>
