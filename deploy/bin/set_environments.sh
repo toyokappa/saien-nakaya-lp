654c4d801b53b2c6bd2190d7ef3704085f8502f1
Yuta Toyokawa <kppg42@gmail.com>
2023-03-01 23:33:00 +0900

    環境設定スクリプトの修正

--- deploy/bin/set_environments.sh
+++ deploy/bin/set_environments.sh
@@ -7,8 +7,8 @@ env=$1
 app_name="saien-nakaya"
 
 # 一旦環境変数へ格納
-=$(aws ssm get-parameters --names /${app_name}/${env}/mailgun_key --with-decryption --query "Parameters[0].Value")
-=$(aws ssm get-parameters --names /${app_name}/${env}/mail_bcc --with-decryption --query "Parameters[0].Value")
+mailgun_key=$(aws ssm get-parameters --names /${app_name}/${env}/mailgun_key --with-decryption --query "Parameters[0].Value")
+mail_bcc=$(aws ssm get-parameters --names /${app_name}/${env}/mail_bcc --with-decryption --query "Parameters[0].Value")
 
 echo "export MAILGUN_KEY=$mailgun_key" >> $BASH_ENV
 echo "export MAIL_BCC=$mail_bcc" >> $BASH_ENV
